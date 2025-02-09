6a59ae4dc3a6f50c38f731a3eade669b324260a5
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2025-02-09 15:28:49 -0400

    atualização tela

---
+++ almsfit/src/app/TELAS/duvidas/page.js
@@ -0,0 +1,28 @@
+import styles from "./duvidas.module.css"
+
+export default function Duvidas(){
+    return(
+        <div className={styles.containerPrincipal}>
+            <h1>Dúvidas</h1>
+            <div className={styles.subContainer}>
+                <div className={styles.info1}>
+
+                    <h3 className={styles.tituloInfo}>Ligue-nos</h3>
+                    <p>(69) 9 9999 - 9999</p>
+
+                    <h3 className={styles.tituloInfo}>Local</h3>
+                    <p>BR-174, Km 3 S/n - Zona Urbana, 
+                    Vilhena - RO, 76980-000</p>
+                    <p>• Ver no google maps</p>
+
+                    <h3>Horários</h3>
+                    <p>Seg - Sex... 6h00 - 22h00 </p>
+                    <p>Sáb - Dom... 10h00 - 18h00</p>
+                </div>
+                
+                <h3 className={styles.info2}>Entre em contato</h3>
+            </div>
+            
+        </div>
+    )
+}
