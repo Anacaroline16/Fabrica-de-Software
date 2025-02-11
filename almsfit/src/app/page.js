c701adbdf4a50f933ee6514fbf49910defed7561
Anacaroline16 <caroline.cordeiro@estudante.ifro.edu.br>
2025-02-11 09:04:36 -0400

    teste

--- almsfit/src/app/page.js
+++ almsfit/src/app/page.js
@@ -1,58 +1,51 @@
 import styles from "./page.module.css"
 import Image from "next/image";
 
+
 export default function Home() {
 	return (
-<<<<<<< HEAD
-		<div className={fundo_home}>
-            <div className={fundo_home_container}>
-                <div className={primeiro_bloco}>
-                    <div className={pmrBloco-coluna1}>
-                        <h1 className={titulo-principal}>
+		<div className={styles.fundo_home}>
+            <div className={styles.fundo_home_container}>
+                <div className={styles.primeiro_bloco}>
+                    <div className={styles.pmrBloco_coluna1}>
+                        <h1 className={styles.titulo_principal}>
                             Transforme seu potencial em realidade: Treine conosco!
                         </h1>
-                        <p className={paragrafo_principal}>Melhore sua saúde com a academia: fortaleça seu corpo, reduza o estresse e viva de forma mais equilibrada e feliz.</p>
-                        <button className={botao_pmrBloco} type="submit" >  
-                                <Image src="img/figura_botao.png" alt="INSCREVA-SE AGORA" />
-                                INSCREVA-SE AGORA
+                        <p className={styles.paragrafo_principal}>Melhore sua saúde com a academia: fortaleça seu corpo, reduza o estresse e viva de forma mais equilibrada e feliz.</p>
+                        <button className={styles.botao_pmrBloco} type="submit" >  
+                                {/* <Image src="img/figura_botao.png" alt="INSCREVA-SE AGORA" /> */}
+                                {/* INSCREVA-SE AGORA */}
                         </button>  
                     </div> 
-                    <div className={pmrBloco_coluna2}>
-                        <Image id="img-ft-principal-coluna2" src="img/img1.png" alt="Mulher malhando na academia" />
+                    <div className={styles.pmrBloco_coluna2}>
+                        {/* <Image id="img-ft-principal-coluna2" src="img/img1.png" alt="Mulher malhando na academia" width={50} height={50} /> */}
                     </div> 
                 </div>
-                <div className={segundo-bloco}>
+                <div className={styles.segundo_bloco}>
                     <h1>O que torna nossa academia diferente?</h1>
-                    <div className={imgs_txt}>
-                        <div className={quadrinho_1}>
-                            <Image src="img/img2_1.png" alt="Equipamentos modernos e variados"/>
+                    <div className={styles.imgs_txt}>
+                        <div className={styles.quadrinho_1}>
+                            {/* <Image src="/images/img2_1.png" alt="Equipamentos modernos e variados" width={50} height={50}/> */}
                             <p>Equipamentos modernos e variados</p>
                         </div>
-                        <div className={quadrinho_1}>
-                            <Image src="img/img2_2.png" alt="Treinamento Personalizado" />
+                        <div className={styles.quadrinho_1}>
+                            {/* <Image src="img/img2_2.png" alt="Treinamento Personalizado" width={50} height={50} /> */}
                             <p>Treinamento Personalizado</p>
                         </div>
-                        <div className={quadrinho_1}>
-                            <Image src="img/img2_3.png" alt="Tecnologia e Apps" />
+                        <div className={styles.quadrinho_1}>
+                            {/* <Image src="/images/" alt="Tecnologia e Apps"  width={50} height={50}/> */}
                             <p>Tecnologia e Apps</p>
                         </div>
                     </div>
                 </div>
-                <div className={terceiro-bloco}>
+                <div className={styles.terceiro_bloco}>
 
-                    <Image class="imgs" src="img/img3_1.png" alt="Mulher treinando com corda naval ao ar livre" />
-                    <Image class="imgs" src="img/img3_2.png" alt="Mulher realizando exercício de pernas em nossa máquina de academia" />
-                    <Image class="imgs" src="img/img3_3.png" alt=" Mulher treinando bícps com halteres em academia" />
+                    {/* <Image className={styles.imgs} src="/images/img3_1.png" alt="Mulher treinando com corda naval ao ar livre" width={50} height={50}/>
+                    <Image className={styles.imgs} src= "/imagesimg/img3_2.png" alt="Mulher realizando exercício de pernas em nossa máquina de academia" width={50} height={50} />
+                    <Image className={styles.imgs} src="/images/img3_3.png" alt=" Mulher treinando bícps com halteres em academia" width={50} height={50}/> */}
 
                 </div>
             </div>
         </div>
 	)
-=======
-		<div>
-			<p>Tela inicial</p>
-			
-		</div>
-	);
->>>>>>> 605ed763dcf02e5750e2484bc0e085c22d6b48b2
 }
