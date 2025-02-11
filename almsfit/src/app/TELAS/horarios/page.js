477b67ab9f0fcf14cf391064d9e91793083e4b7d
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-02-11 09:49:39 -0400

    salvarcontato

--- almsfit/src/app/TELAS/horarios/page.js
+++ almsfit/src/app/TELAS/horarios/page.js
@@ -22,7 +22,7 @@ export default function Horário() {
           <p className="TituloMaior">Feriados e eventos:</p>
           <p className="subtitulo">06h00 à 12h00</p>
         </div>
-        <img src="./img/Vector (11).png" className="time" alt="Relógio" />
+        <Image src="./img/Vector (11).png" className="time" alt="Relógio" />
       </main>
     </div>
   );
