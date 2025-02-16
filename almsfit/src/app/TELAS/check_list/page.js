eb71e9d21532bfa072e64dc3a7aec4d2def9067c
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-02-16 16:49:03 -0400

    TeladeChecklist

--- almsfit/src/app/TELAS/check_list/page.js
+++ almsfit/src/app/TELAS/check_list/page.js
@@ -1,74 +1,158 @@
-// <!DOCTYPE html>
-// <html lang="en">
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./checklist.module.css";
 
-// <head>
-//   <meta charset="UTF-8">
-//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//   <link rel="stylesheet" href="css/style.css">
-//   <link rel="stylesheet" href="./css/header.css">
-//   <link rel="stylesheet" href="./css/footer.css">
-//   <link rel="stylesheet" href="./js/script.js">
-//   <link rel="stylesheet" href="./css/footer.css">
-//   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-//   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-//   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-//   <link rel="manifest" href="/site.webmanifest">
-//   <link rel="shortcut icon" href="../../Telas SOFIA/Duvida/img/ALMS FIT (Site) (4).png" type="image/x-icon">
-//   <script src="https://kit.fontawesome.com/df40682070.js" crossorigin="anonymous"></script>
-//   <title>ALMS Fit</title>
-// </head>
+const exercicios = [
+    { id: 1, nome: "Leg press 45°", series: "4", reps: "12", img: "/images/legpress45.png" },
+    { id: 2, nome: "Adutor", series: "4", reps: "15", img: "/images/27 1.png" },
+    { id: 3, nome: "Panturrilha", series: "4", reps: "12", img: "/images/26 1.png" },
+    { id: 4, nome: "Agachamento", series: "4", reps: "12", img: "/images/28 1.png" },
+    { id: 5, nome: "Búlgaro", series: "4", reps: "12", img: "/images/29 2.png" },
+  ];
 
-// <header>
-//   <nav>
-//       <div class="menu">
-//           <div><img src="../../design/img/Sobre (12) 2.png " alt="Logo"></div>
-//           <div class="ancoras">
-//               <ul> 
-//                   <li><a href="../Telas ANA/home/index.html">Home</a></li>
-//                   <li><a href="../../Telas Maria/horário/index.html">Horários</a></li>
-//                   <li><a href="../../abrica/escolha_cad.html">Treinos</a></li>
-//                   <li><a href="../../Telas Maria/contato/index.html">Contato</a></li>
-//               </ul>                   
-//           </div>
-//           <img  src="../../design/img/Sobre (13) 2.png" class="area">    
+  const exercicios_02 = [
+    { id: 1, nome: "Rosca Unilateral", series: "4", reps: "12", img: "/images/30 1.png" },
+    { id: 2, nome: "Abdutor", series: "4", reps: "15", img: "/images/31 1.png" },
+    { id: 3, nome: "Remada", series: "4", reps: "12", img: "/images/33 1.png" },
+    { id: 4, nome: "Supino", series: "4", reps: "12", img: "/images/32 1.png" },
+    { id: 5, nome: "Tríceps", series: "4", reps: "12", img: "/images/34 1.png" },
+  ]; 
+
+
+const Checklist = () => {
+  const [checkedItems, setCheckedItems] = useState({});
+
+  const toggleCheck = (id) => {
+    setCheckedItems((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
+
+  const desmarcarTudo = () => {
+    setCheckedItems({});
+  };
+
+  return (
+    <main className={styles.container}>
+        <Image src="/images/ALMS FIT (1) 1.png" className={styles.checkzinho} 
+            width={80}
+             height={50}
+            alt="Quadrado vermelho"/>
+      <h1>CHECKLIST</h1>
+      <button className={styles.funcButton}>*Alteração apenas funcionários</button>
+      <ul className={styles.checklist}>
+        {exercicios.map((exercicio) => (
+          <li key={exercicio.id}>
+            <input
+              type="checkbox"
+              id={`item${exercicio.id}`}
+              checked={checkedItems[exercicio.id] || false}
+              onChange={() => toggleCheck(exercicio.id)}
+            />
+            <label htmlFor={`item${exercicio.id}`}>
+              {exercicio.nome} <br />
+              Séries: {exercicio.series} Rep: {exercicio.reps}
+              <img src={exercicio.img} alt={exercicio.nome} />
+            </label>
+          </li>
+        ))}
+      </ul>
+
+      <ul className={styles.checklist_02}>
+        {exercicios_02.map((exercicio) => (
+          <li className={styles.checklist_02li} key={exercicio.id}>
+            <input
+              type="checkbox"
+              id={`item${exercicio.id}`}
+              checked={checkedItems[exercicio.id] || false}
+              onChange={() => toggleCheck(exercicio.id)}
+            />
+            <label htmlFor={`item${exercicio.id}`}>
+              {exercicio.nome} <br />
+              Séries: {exercicio.series} Rep: {exercicio.reps}
+              <img src={exercicio.img} alt={exercicio.nome} />
+            </label>
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.containerimg1}>
+            <Image src="/images/Rectangle 96 (1).png" className={styles.quadradored01} 
+            width={350}
+             height={350}
+            alt="Quadrado vermelho"/>
+            </div>
+      
+         <div className={styles.containerImg2}>
+            <Image src="/images/Rectangle 96 (1).png" className={styles.quadradored02} 
+            width={350}
+            height={350}
+            alt="Quadrado vermelho 02"/>  
+        </div>
+      <button onClick={desmarcarTudo} className={styles.clearButton}>Desmarcar Tudo</button>
+    </main>
+  );
+};
+
+export default Checklist;
+
+
+
+
+
+
+
+// const exercicios = [
+//   { id: 1, nome: "Leg press 45°", series: "4", reps: "12", img: "/images/legpress45.png" },
+//   { id: 2, nome: "Adutor", series: "4", reps: "15", img: "/images/27 1.png" },
+//   { id: 3, nome: "Panturrilha", series: "4", reps: "12", img: "/images/26 1.png" },
+//   { id: 4, nome: "Agachamento", series: "4", reps: "12", img: "/images/28 1.png" },
+//   { id: 5, nome: "Búlgaro", series: "4", reps: "12", img: "/images/29 2.png" },
+//   { id: 6, nome: "Rosca Unilateral", series: "2", reps: "12", img: "/images/30 1.png" },
+//   { id: 7, nome: "Abdutor", series: "4", reps: "15", img: "/images/31 1.png" },
+//   { id: 8, nome: "Remada", series: "2", reps: "8", img: "/images/33 1.png" },
+//   { id: 9, nome: "Supino", series: "2", reps: "8", img: "/images/32 1.png" },
+//   { id: 10, nome: "Trícpes", series: "4", reps: "12", img: "/images/34 1.png" },
+// ];
+
+// const Checklist = () => {
+//   return (
+//     <main className={styles.container}>
+//          <Image src="/images/ALMS FIT (1) 1.png" className={styles.checkzinho} 
+//             width={80}
+//             height={50}
+//             alt="check"/>
+//       <h1>CheckList</h1>
+//       <button className={styles.funcButton}>*Alteração apenas funcionários</button>
+
+//       <div className={styles.containerimg1}>
+//       <Image src="/images/Rectangle 96 (1).png" className={styles.quadradored01} 
+//             width={350}
+//             height={350}
+//             alt="Quadrado vermelho"/>
 //       </div>
-//   </nav>
-//   <div class="borda"></div>  
-// </header>
+      
+//         <div className={styles.containerImg2}>
+//         <Image src="/images/Rectangle 96 (1).png" className={styles.quadradored02} 
+//             width={350}
+//             height={350}
+//             alt="Quadrado vermelho 02"/>  
+//         </div>
 
-// <html>
-// <body>
-//   <main>
-//     <h1>CHECKLIST</h1>
-//     <div class="button_alter">
-//       <button type="submit">*Alteração apenas funcionários</button>
-//     </div>
-//     <div class="checklist-container">
-//       <ul class="checklist">
-//         <li><input type="checkbox" id="item1"><label for="item1">Leg press 45°<p class="center">Séries: 4 Rep: 12 <img src="./img/ALMS FIT 1.png" class="img1"></label></li>
-//         <li><input type="checkbox" id="item2"><label for="item2">Adutor <br>Séries: 4 Rep: 15 <img src="./img/27 1.png" class="img2"></label></li>
-//         <li><input type="checkbox" id="item3"><label for="item3">Panturilha <br>Séries: 4 Rep: 12 <img src="./img/26 1.png" class="img3"></label></li>
-//         <li><input type="checkbox" id="item4"><label for="item4">Agachamento <br>Séries: 4 Rep: 12 <img src="./img/28 1.png" class="img4"></label></li>
-//         <li><input type="checkbox" id="item5"><label for="item4">Flexora <br>Séries: 4 Rep: 12 <img src="./img/29 2.png" class="img5"></label></li>
-//       </ul>
+//       <div className={styles.grid}>
+//         {exercicios.map((exercicio) => (
+//           <div key={exercicio.id} className={styles.card}>
+//             <img src={exercicio.img} alt={exercicio.nome} className={styles.exercicioImg}  width={90}
+//             height={60} />
+//             <h3>{exercicio.nome}</h3>
+//             <p>Séries: {exercicio.series} | Reps: {exercicio.reps}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </main>
+//   );
+// };
 
-//       <ul class="checklist1">
-//         <li><input type="checkbox" id="item6"><label for="item6">Rosca Unilateral<br>Séries: 2 Rep: 12 <img src="./img/30 1.png" class="img6"></label></li>
-//         <li><input type="checkbox" id="item7"><label for="item7">Abdutor <br>Séries: 4 Rep: 15 <img src="./img/31 1.png" class="img7"></label></li>
-//         <li><input type="checkbox" id="item8"><label for="item8">Remada <br>Séries: 4 Rep: 12 <img src="./img/33 1.png" class="img8"></label></li>
-//         <li><input type="checkbox" id="item9"><label for="item9">Supino <br>Séries: 4 Rep: 12 <img src="./img/32 1.png" class="img9"></label></li>
-//         <li><input type="checkbox" id="item10"><label for="ite10">Tríceps <br>Séries: 4 Rep: 12 <img src="./img/34 1.png" class="img10"></label></li>
-//       </ul>
+// export default Checklist;
 
-//     </div>
-//     <button onclick="desmarcarTudo()">Desmarcar Tudo</button>
-//     <script>
-//       function desmarcarTudo() {
-//         const checkboxes = document.querySelectorAll('.checklist-container input[type="checkbox"]');
-//         checkboxes.forEach(checkbox => checkbox.checked = false);
-//       }
-//     </script>
-//   </main>
 
-// </body>
-// </html>
+       
