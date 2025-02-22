c7e422f607eb35e4a8fe1f2f11473dd327de1dca
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2025-02-22 14:52:56 -0400

    atualizacao check_list

--- almsfit/src/app/TELAS/check_list/page.js
+++ almsfit/src/app/TELAS/check_list/page.js
@@ -4,20 +4,20 @@ import Image from "next/image";
 import styles from "./checklist.module.css";
 
 const exercicios = [
-    { id: 1, nome: "Leg press 45°", series: "4", reps: "12", img: "/images/legpress45.png" },
-    { id: 2, nome: "Adutor", series: "4", reps: "15", img: "/images/27 1.png" },
-    { id: 3, nome: "Panturrilha", series: "4", reps: "12", img: "/images/26 1.png" },
-    { id: 4, nome: "Agachamento", series: "4", reps: "12", img: "/images/28 1.png" },
-    { id: 5, nome: "Búlgaro", series: "4", reps: "12", img: "/images/29 2.png" },
-  ];
+  { id: 1, nome: "Leg press 45°", series: "4", reps: "12", img: "/images/legpress45.png" },
+  { id: 2, nome: "Adutor", series: "4", reps: "15", img: "/images/27 1.png" },
+  { id: 3, nome: "Panturrilha", series: "4", reps: "12", img: "/images/26 1.png" },
+  { id: 4, nome: "Agachamento", series: "4", reps: "12", img: "/images/28 1.png" },
+  { id: 5, nome: "Búlgaro", series: "4", reps: "12", img: "/images/29 2.png" },
+];
 
-  const exercicios_02 = [
-    { id: 1, nome: "Rosca Unilateral", series: "4", reps: "12", img: "/images/30 1.png" },
-    { id: 2, nome: "Abdutor", series: "4", reps: "15", img: "/images/31 1.png" },
-    { id: 3, nome: "Remada", series: "4", reps: "12", img: "/images/33 1.png" },
-    { id: 4, nome: "Supino", series: "4", reps: "12", img: "/images/32 1.png" },
-    { id: 5, nome: "Tríceps", series: "4", reps: "12", img: "/images/34 1.png" },
-  ]; 
+const exercicios_02 = [
+  { id: 1, nome: "Rosca Unilateral", series: "4", reps: "12", img: "/images/30 1.png" },
+  { id: 2, nome: "Abdutor", series: "4", reps: "15", img: "/images/31 1.png" },
+  { id: 3, nome: "Remada", series: "4", reps: "12", img: "/images/33 1.png" },
+  { id: 4, nome: "Supino", series: "4", reps: "12", img: "/images/32 1.png" },
+  { id: 5, nome: "Tríceps", series: "4", reps: "12", img: "/images/34 1.png" },
+];
 
 
 const Checklist = () => {
@@ -33,63 +33,56 @@ const Checklist = () => {
 
   return (
     <main className={styles.container}>
-        <Image src="/images/ALMS FIT (1) 1.png" className={styles.checkzinho} 
-            width={80}
-             height={50}
-            alt="check"/>
+      <Image src="/images/ALMS FIT (1) 1.png" className={styles.checkzinho}
+        width={80}
+        height={50}
+        alt="check" />
       <h1>CHECKLIST</h1>
       <button className={styles.funcButton}>*Alteração apenas funcionários</button>
-      <ul className={styles.checklist}>
-        {exercicios.map((exercicio) => (
-          <li key={exercicio.id}>
-            <input
-              type="checkbox"
-              id={`item${exercicio.id}`}
-              checked={checkedItems[exercicio.id] || false}
-              onChange={() => toggleCheck(exercicio.id)}
-            />
-            <label className={styles.label}
-             htmlFor={`item${exercicio.id}`}>
-              {exercicio.nome} <br />
-              Séries: {exercicio.series} Rep: {exercicio.reps}
-              <img src={exercicio.img} alt={exercicio.nome} />
-            </label>
-          </li>
-        ))}
-      </ul>
 
-      <ul className={styles.checklist_02}>
-        {exercicios_02.map((exercicio) => (
-          <li className={styles.checklist_02li} key={exercicio.id}>
-            <input
-              type="checkbox"
-              id={`item${exercicio.id}`}
-              checked={checkedItems[exercicio.id] || false}
-              onChange={() => toggleCheck(exercicio.id)}
-            />
-            <label className={styles.label}
-            htmlFor={`item${exercicio.id}`}>
-              {exercicio.nome} <br />
-              Séries: {exercicio.series} Rep: {exercicio.reps}
-              <img src={exercicio.img} alt={exercicio.nome} />
-            </label>
-          </li>
-        ))}
-      </ul>
+      <div className={styles.fundo_checkList}>
+        <ul className={styles.checklist}>
+          {exercicios.map((exercicio) => (
+            <li key={exercicio.id}>
+              <input
+                type="checkbox"
+                id={`item${exercicio.id}`}
+                checked={checkedItems[exercicio.id] || false}
+                onChange={() => toggleCheck(exercicio.id)}
+              />
+              <label className={styles.label}
+                htmlFor={`item${exercicio.id}`}>
+                {exercicio.nome} <br />
+                Séries: {exercicio.series} Rep: {exercicio.reps}
+                <img src={exercicio.img} alt={exercicio.nome} />
+              </label>
+            </li>
+          ))}
+        </ul>
 
-      <div className={styles.containerimg1}>
-            <Image src="/images/Rectangle 96 (1).png" className={styles.quadradored01} 
-            width={400}
-            height={410}
-            alt="Quadrado vermelho"/>
-            </div>
-      
-         <div className={styles.containerImg2}>
-            <Image src="/images/Rectangle 96 (1).png" className={styles.quadradored02} 
-            width={400}
-            height={410}
-            alt="Quadrado vermelho 02"/>  
-        </div>
+        <ul className={styles.checklist_02}>
+          {exercicios_02.map((exercicio) => (
+            <li className={styles.checklist_02li} key={exercicio.id}>
+              <input
+                type="checkbox"
+                id={`item${exercicio.id}`}
+                checked={checkedItems[exercicio.id] || false}
+                onChange={() => toggleCheck(exercicio.id)}
+              />
+              <label className={styles.label}
+                htmlFor={`item${exercicio.id}`}>
+                {exercicio.nome} <br />
+                Séries: {exercicio.series} Rep: {exercicio.reps}
+                <img src={exercicio.img} alt={exercicio.nome} />
+              </label>
+            </li>
+          ))}
+        </ul>
+
+
+      </div>
+
+
       <button onClick={desmarcarTudo} className={styles.clearButton}>Desmarcar Tudo</button>
     </main>
   );
